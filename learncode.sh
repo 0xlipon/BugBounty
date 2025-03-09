82bcfd66accb78183b51253a4825e09d16a826d5
Md. Torikul Islam Lipon <124679777+0xlipon@users.noreply.github.com>
2025-03-09 13:09:25 +0600

    Delete learncode.sh

--- learncode.sh
+++
@@ -1,7 +0,0 @@
-HTML → w3schools.com
-CSS → CSS-tricks.com
-JavaScript → LearnJavaScript.online
-Git and GitHub → LearnGitBranching.js.org
-React → React-tutorial.app
-SQL → SQLbolt.com
-Blockchain → cryptozombies.io 
